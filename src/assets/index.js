3eb11a6fa0d55a3dc844a37159d977d32f0955df
Subasinghe S.A.B.D <banukasubasinghe13@gmail.com>
2023-12-19 21:36:56 +0530

    committs

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,8 @@
 import img_header from './header.png';
+import logo from './logo.png'
 import img_search from './search.png'
 import bars from './bars.svg'
+import screen from './screen.png'
+import search from './seacrch.png'
 
-export {bars,img_header,img_search}
+export {search,screen,logo,bars,img_header,img_search}
